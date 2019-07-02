2306106b561519c080c1d60c2fa466dac2e19f41
Kieran Schubert <schubert.kieran@gmail.com>
2019-07-02 14:19:44 +0200

    Last commit

--- portfolio_optim.R
+++ portfolio_optim.R
@@ -8,8 +8,8 @@ symbols <- c("AAPL", "GOOG")
 
 getSymbols(symbols, from = "2018-01-01", to = Sys.Date(), src =  "yahoo", adjust =  TRUE)
 
-AAPL.ret <- dailyReturn(AAPL$AAPL.Close)
-GOOG.ret <- dailyReturn(GOOG$GOOG.Close)
+AAPL.ret <- dailyReturn(na.omit(AAPL$AAPL.Close))
+GOOG.ret <- dailyReturn(na.omit(GOOG$GOOG.Close))
 rets <- cbind(AAPL.ret$daily.returns, GOOG.ret$daily.returns)
 
 mean.ret <- c(mean(AAPL.ret), mean(GOOG.ret))
@@ -18,4 +18,5 @@ cov.matrix <- cov(rets)
 omega.star <- (cov.matrix[2,2] - cov.matrix[2,1])/(cov.matrix[1,1] + cov.matrix[2,2]
                                                    - 2*cov.matrix[2,1])
 
+omega.star
 # Invest 41% in AAPL and 59% in GOOG
