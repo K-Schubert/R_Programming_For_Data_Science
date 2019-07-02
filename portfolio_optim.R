e8c5a67fe1085f9bacc15a250b965af6cfe77971
Kieran Schubert <schubert.kieran@gmail.com>
2019-07-02 14:18:02 +0200

    Portfolio optimization for 2 stocks

---
+++ portfolio_optim.R
@@ -0,0 +1,22 @@
+install.packages('quantmod')
+library(quantmod)
+library(magrittr)
+library(tidyr)
+library(dplyr)
+
+symbols <- c("AAPL", "GOOG")
+
+getSymbols(symbols, from = "2018-01-01", to = Sys.Date(), src =  "yahoo", adjust =  TRUE)
+stocks <- cbind(AAPL$AAPL.Close, GOOG$GOOG.Close)
+
+AAPL.ret <- dailyReturn(AAPL$AAPL.Close)
+GOOG.ret <- dailyReturn(GOOG$GOOG.Close)
+rets <- cbind(AAPL.ret$daily.returns, GOOG.ret$daily.returns)
+
+mean.ret <- c(mean(AAPL.ret), mean(GOOG.ret))
+cov.matrix <- cov(rets)
+
+omega.star <- (cov.matrix[2,2] - cov.matrix[2,1])/(cov.matrix[1,1] + cov.matrix[2,2]
+                                                   - 2*cov.matrix[2,1])
+
+# Invest 41% in AAPL and 59% in GOOG
