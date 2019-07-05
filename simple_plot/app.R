d18567469e2094a928b5e6b0340f5609b2668e9d
Kieran Schubert <schubert.kieran@gmail.com>
2019-07-05 11:25:54 +0200

    first commit

---
+++ simple_plot/app.R
@@ -0,0 +1,42 @@
+
+library(shiny)
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Old Faithful Geyser Data"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            selectInput("color", label = h3("Choose color"), 
+                        choices = list("Red" = 'red', "Green" = 'green', "Blue" = 'blue'), 
+                        selected = 1),
+            
+            hr(),
+            fluidRow(column(3, verbatimTextOutput("value")))
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("distPlot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+        
+        x <- 1:10
+        plot(x, col=input$color, type='l')
+
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
