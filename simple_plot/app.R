c8003dc7b6033d3c125ab78b101a3e65202d391f
Kieran Schubert <schubert.kieran@gmail.com>
2019-07-05 11:26:57 +0200

    change title

--- simple_plot/app.R
+++ simple_plot/app.R
@@ -6,7 +6,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Simple plot"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
